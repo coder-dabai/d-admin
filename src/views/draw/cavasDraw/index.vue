8e9e09e2ff59b66734817810aaddc435f78bcb15
coder-dabai <631389205@qq.com>
2023-06-13 09:51:27 +0800

    fix: 'ftp 邮件错误推送'

--- src/views/draw/cavasDraw/index.vue
+++ src/views/draw/cavasDraw/index.vue
@@ -8,6 +8,13 @@
           </div>
         </template>
       </ImgWrapper>
+      <ImgWrapper>
+        <template #content>
+          <div class="flex justify-center">
+            <canvas width="350" height="350" ref="circle"></canvas>
+          </div>
+        </template>
+      </ImgWrapper>
     </div>
   </PageWrapper>
 </template>
@@ -18,8 +25,9 @@
   import headerImg from '/@/assets/images/header.jpg';
   import wechat from '/@/assets/images/wechat.jpg';
 
+  // 画个海报
   const cas = ref<any>(null);
-  const init = () => {
+  const drawPoster = () => {
     let ctx = cas.value.getContext('2d');
     ctx.fillStyle = '#fff';
     ctx.fillRect(0, 0, 350, 350);
@@ -52,7 +60,72 @@
     };
   };
 
+  // 绘制圆圈百分比加载
+  const circle = ref<any>(null);
+  let circleCtx: any;
+  let centerX;
+  let centerY;
+  let rad; // 圆分为100份
+  let speed;
+  const circleCtxInit = () => {
+    circleCtx = circle.value.getContext('2d');
+    console.log('hahah', circleCtx);
+    centerX = circleCtx.width;
+    centerY = circleCtx.height;
+    rad = (Math.PI * 2) / 100; // 圆分为100份
+    speed = 0.1;
+  };
+
+  // 绘制文本
+  const drawCircleText = (n) => {
+    circleCtx.fillStyle = '#F47C7C';
+    circleCtx.font = '40px Arial';
+    circleCtx.textAlign = 'center';
+    circleCtx.textBaseline = 'middle';
+    circleCtx.fillText(n.toFixed(0) + '%', centerX, centerY);
+    console.log('绘制文本');
+  };
+
+  // 绘制蓝色外圈
+  const drawBlueCircle = (n) => {
+    circleCtx.beginPath();
+    circleCtx.strokeStyle = '#49f';
+    circleCtx.lineWidth = 12;
+    circleCtx.arc(centerX, centerY, 100, -Math.PI / 2, -Math.PI / 2 + n * rad, false);
+    circleCtx.stroke();
+    console.log('绘制蓝色外圈');
+  };
+
+  // 绘制白色外圈
+  const drawWhiteCiecle = () => {
+    circleCtx.beginPath();
+    circleCtx.strokeStyle = '#A5DEF1';
+    circleCtx.lineWidth = 12;
+    circleCtx.arc(centerX, centerY, 100, 0, Math.PI * 2, false);
+    circleCtx.stroke();
+    circleCtx.closePath();
+    console.log('绘制白色外圈');
+  };
+
+  const drawCircelLoading = () => {
+    console.log('111');
+    window.requestAnimationFrame(drawCircelLoading);
+    circleCtx.clearRect(0, 0, circleCtx.width, circleCtx.height);
+
+    drawWhiteCiecle();
+    drawCircleText(speed);
+    drawBlueCircle(speed);
+
+    if (speed > 100) speed = 0;
+    speed += 0.1;
+    console.log('结束');
+  };
+
   onMounted(() => {
-    init();
+    drawPoster();
+    circleCtxInit();
+    setTimeout(() => {
+      drawCircelLoading();
+    }, 1000);
   });
 </script>
